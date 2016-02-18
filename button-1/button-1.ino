16f71d1bcab74d240010c28e4f2a7e4842d7a860
Eelco Dijkstra <eelco.j.dijkstra@gmail.com>
2016-02-18 08:12:27 +0100

    Simplify Button-1

--- button-1/button-1.ino
+++ button-1/button-1.ino
@@ -3,9 +3,6 @@ const int onButtonPin = 2;     // pin nr of "on" button
 const int offButtonPin = 3;    // pin nr of "off" button
 const int ledPin =  13;        // pin nr of LED
 
-int onButtonState = 0;         // variable for reading "on" button
-int offButtonState = 0;        // variable for reading "off" button
-
 void setup() {
   pinMode(ledPin, OUTPUT);      // initialize LED pin as output
   pinMode(onButtonPin, INPUT);  // initialize button pin as input
@@ -13,13 +10,11 @@ void setup() {
 }
 
 void loop() {
-  onButtonState = digitalRead(onButtonPin);
-  if (onButtonState == HIGH) {
+  if (digitalRead(onButtonPin) == HIGH) {
     digitalWrite(ledPin, HIGH);
   }
 
-  offButtonState = digitalRead(offButtonPin);
-  if (offButtonState == HIGH) {
+  if (digitalRead(offButtonPin) == HIGH) {
     digitalWrite(ledPin, LOW);
   }
 }
